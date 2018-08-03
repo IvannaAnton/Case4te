f74bacdeda6ea71d6597ec3990d888b27f25a96c
BrianGuadron <brianguadron@gmail.com>
2018-08-03 11:30:38 -0400

    all test case done

--- src/test/java/gov/healthdata/tests/TC002.java
+++ src/test/java/gov/healthdata/tests/TC002.java
@@ -6,16 +6,15 @@ import org.testng.annotations.Test;
 
 import gov.healthdata.pages.HealthDataMainPage;
 import gov.healthdata.utilities.ConfigurationReader;
+import gov.healthdata.utilities.Driver;
 
 public class TC002 extends TestBase {
 	
 	@Test(priority = 2, groups= {"smoke"})
 	public void verifySearchBoxInHomePageTest() {
 		
-		driver.get(ConfigurationReader.getProperty("url"));
-		actual =driver.getTitle();
-		expected = "HealthData.gov";
-		assertEquals(expected, actual);
+		
+		assertTrue(Driver.getDriver().getCurrentUrl().contains("www.healthdata.gov"));
 		HealthDataMainPage mainPage =  new HealthDataMainPage();
 		assertTrue(mainPage.searchField.isDisplayed());
 	
