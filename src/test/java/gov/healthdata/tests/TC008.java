ba539ef28aca36edbe0313301729186c77014e4b
4-Vyper-4 <charles.s.cybertek@gmail.com>
2018-08-04 15:37:15 -0400

    TC008=Blocked, TC016=Blocked, TC020=Completed/NeedsReview

--- src/test/java/gov/healthdata/tests/TC008.java
+++ src/test/java/gov/healthdata/tests/TC008.java
@@ -1,5 +1,21 @@
 package gov.healthdata.tests;
 
-public class TC008 {
+import static org.testng.Assert.assertTrue;
 
+import org.testng.annotations.Test;
+
+import gov.healthdata.pages.HealthDataMainPage;
+import gov.healthdata.utilities.Driver;
+
+public class TC008 extends TestBase {
+	
+	HealthDataMainPage healthDataMainPage = new HealthDataMainPage();
+
+	@Test
+	public void autoSuggestBox() {
+		assertTrue(Driver.getDriver().getTitle().contains("HealthData.gov"));
+		healthDataMainPage.searchField.sendKeys("he");
+		//nothing to verify
+		//auto suggestion does not exist
+	}
 }
--- src/test/java/gov/healthdata/tests/TC020.java
+++ src/test/java/gov/healthdata/tests/TC020.java
@@ -1,6 +1,27 @@
 package gov.healthdata.tests;
 
-public class TC020 {
-//pushing to branch	
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import gov.healthdata.pages.HealthDataMainPage;
+import gov.healthdata.pages.HealthDataSearchResultPage;
+import gov.healthdata.utilities.Driver;
+
+public class TC020 extends TestBase {
+
+	HealthDataMainPage healthDataMainPage = new HealthDataMainPage();
+	HealthDataSearchResultPage healthDataSearchResultPage = new HealthDataSearchResultPage();
+
+	@Test
+	public void incorrectDataSearch() {
+		assertTrue(Driver.getDriver().getTitle().contains("HealthData.gov"));
+		healthDataMainPage.searchField.sendKeys("halth");
+		healthDataMainPage.searchButton.click();
+		assertEquals((healthDataSearchResultPage.notFoundMessage.getText()),
+				"No results were found. Please try another keyword.");
+
+	}
 
 }
