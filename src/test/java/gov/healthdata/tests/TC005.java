943956492ea57ae08b6790aaa5267761192224a5
IvannaAnton <ivanna.anton@aiesec.net>
2018-08-06 15:50:04 -0400

    new merge

--- src/test/java/gov/healthdata/tests/TC005.java
+++ src/test/java/gov/healthdata/tests/TC005.java
@@ -1,9 +1,11 @@
 package gov.healthdata.tests;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.Test;
 
+import gov.healthdata.pages.HealthDataMainPage;
 import gov.healthdata.utilities.Driver;
 
 public class TC005 extends TestBase{
@@ -11,5 +13,6 @@ public class TC005 extends TestBase{
 	@Test
 	public void canNotAutomateTest5() {
 		assertTrue(Driver.getDriver().getCurrentUrl().contains("www.healthdata.gov"));
+		
 	}
 }
